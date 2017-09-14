9942976719ebe0a51acb0fdaf0256963befcf301
javacasm <javacasm@gmail.com>
2017-09-14 09:44:22 +0200

    plantilla

---
+++ Plantillas/Plantilla_codigo.ino
@@ -0,0 +1,25 @@
+/*
+ Nombre del circuito
+ Autor
+
+ Descripción
+
+ Hardware que se usa
+ */
+
+// constantes que no cambian
+// se usan para establecer los pines
+
+const int buttonPin = 2;     // el numero del pin del pulsador
+
+// variables globales que cambian
+int buttonState = 0;         // variable para almacenar el estado del pulsador
+
+// Configuración
+void setup() {
+
+}
+
+// Bucle que se repite
+void loop() {
+}
