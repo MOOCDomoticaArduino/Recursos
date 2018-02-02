7f644b648207dd918de1e070d3a992b79fd3911d
javacasm <javacasm@gmail.com>
2018-02-02 14:06:51 +0100

    codigo

---
+++ Tema 8/8.4_Funciones/8.4_Funciones.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+
+int funcionSuma(int sumando1,int sumando2){
+  int resultado;
+
+  resultado=sumando1+sumando2;
+  return resultado
+}
+
+
+void loop() {
+  int i=2;
+  int j=3;
+
+  int suma=funcionSuma(i,j);
+}
