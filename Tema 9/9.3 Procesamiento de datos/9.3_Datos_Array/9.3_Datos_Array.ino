e4a17851d29193a51d986969a86c0f068ea63153
javacasm <javacasm@gmail.com>
2018-02-14 12:42:26 +0100

    codigo

---
+++ Tema 9/9.3 Procesamiento de datos/9.3_Datos_Array/9.3_Datos_Array.ino
@@ -0,0 +1,23 @@
+
+
+const int NumOfData=10;
+
+void setup() {
+}
+
+void loop() {
+  int data[NumOfData];
+  
+  for(int i = 0 ; i < NumOfData ; i++ ){
+      data[i]=analogRead(A0); // Lectura del sensor
+  }
+
+  int maxValue=data[0];       
+  int minValue=data[0];
+
+  for(int i = 0 ; i < NumOfData ; i++ ){
+    if( maxValue < data[i]) maxValue = data[i];
+    if( minValue > data[i]) minValue = data[i];
+  }
+  
+}
