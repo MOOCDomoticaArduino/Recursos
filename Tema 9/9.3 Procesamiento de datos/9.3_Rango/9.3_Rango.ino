aa4636e657adae51f5546a9aa0eb2b228afe8ce3
javacasm <javacasm@gmail.com>
2018-02-14 12:44:16 +0100

    Rango

---
+++ Tema 9/9.3 Procesamiento de datos/9.3_Rango/9.3_Rango.ino
@@ -0,0 +1,26 @@
+
+const int LDRPin = A0;
+
+const long CalibrationTime=1000;
+
+int maxValue=0;       // Nos aseguramos que se calculen bien
+int minValue=10000;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Calibrando. Modifique la iluminacion");
+  long startCalibrationTime=millis();
+  while( ( millis() - startCalibrationTime ) < CalibrationTime ){
+    int ldrValue = analogRead(LDRPin);
+    if( maxValue < ldrValue) maxValue = ldrValue;
+    if( minValue > ldrValue) minValue = ldrValue;
+  }
+  
+
+}
+
+void loop() {
+  // Ahora podemos reescalar los valroes entre minValue y maxValue
+  int valorEscalado = map( analogRead(LDRPin), minValue, maxValue,0,255);
+
+}
